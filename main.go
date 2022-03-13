fe0256a225675dd223f40ddf94cd4934d738063e
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Type Inference

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, z)
+  // v := 42
+	v := 42.0
+	fmt.Printf("v is of type %T\n", v)
 }
