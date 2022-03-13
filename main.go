6e4e5164ff1faf64c2fde9e76c04ea93cd60a9bd
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Interfaces are implemented implicitly

--- main.go
+++ main.go
@@ -2,45 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-type Abser interface {
-	Abs() float64
+type I interface {
+	M()
 }
 
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
+}
 
 func main() {
-	var a Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3,4}
-
-	a = f
-	a = &v
-
-	// this line doesn't compile because the receiver in Abs() is a pointer
-	// either change the receiver to accept a value or pass a pointer
-	// a = v
-
-	fmt.Println(a.Abs())
+	var i I = T{"Hello"}
+	i.M()
 }
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y int
-}
-
-func (v *Vertex) Abs() float64 {
-	// the commented line, 19, doesn't compile because this accepts a pointer
-	// either change the receiver to accept a value or pass a pointer
-	return math.Sqrt(float64(v.X*v.X + v.Y*v.Y))
-}
-
