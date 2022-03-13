5ddd1b02e4a68412858ac5461527aea87970d5fe
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:42 -0500

    Short Variable Declarations

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 )
 
-// A var declaration can include initializers, one per variable.
-// if an initializer is present, the type can be omitted; the variable will take the type of the initializer.
-var i, j int = 1, 2
-
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, c, python, java)
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
