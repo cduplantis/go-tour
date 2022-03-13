f6914d9319f4fda1f57ee4f55f11ad49ca27e9d2
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Pointers

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("counting")
+	i, j :=  42, 2701 // short variable declaration
+	p := &i
+	fmt.Println(*p) // read i through the pointer p
+	*p = 21 // set i through the pointer p
+	fmt.Println(i) // see the new value of i
 
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("done")
+	p = &j // point to j
+	*p = *p / 37 // divide j through the pointer p
+	fmt.Println(j)
 }
