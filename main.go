b5abe044f4c633001d3c4962a2d8f1c5b77bb2aa
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Structs

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
-	i, j :=  42, 2701 // short variable declaration
-	p := &i
-	fmt.Println(*p) // read i through the pointer p
-	*p = 21 // set i through the pointer p
-	fmt.Println(i) // see the new value of i
+type Vertex struct {
+	X int
+	Y int
+}
 
-	p = &j // point to j
-	*p = *p / 37 // divide j through the pointer p
-	fmt.Println(j)
+func main() {
+	fmt.Printf("%v\n", Vertex{1, 2})
 }
