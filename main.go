66d17a7fe176a0747de2957cbe8960679c513d7f
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:42 -0500

    Playground

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-  fmt.Println("Hello, Casey")
+  fmt.Println("Welcome to the playground!")
+
+  fmt.Println("The time is", time.Now())
 }
