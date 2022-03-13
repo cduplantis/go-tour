3f9bcb02e04df7a952230edd601c7c1e48e5c06e
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Interface values

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type I interface {
@@ -16,7 +17,23 @@ func (t T) M() {
 	fmt.Println(t.S)
 }
 
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
 func main() {
-	var i I = T{"Hello"}
+	var i I
+	i = T{"Hello"}
+	describe(i)
 	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I){
+	fmt.Printf("(%v, %T)\n", i, i)
 }
