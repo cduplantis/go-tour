5a0ce2f04ecd309d3f382e21aac824019e515389
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Maps

--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
-import (
-	"golang.org/x/tour/pic"
-)
+import "fmt"
 
-func Pic(dx, dy int) [][]uint8 {
-	p := make([][]uint8, dy)
-	for y := 0; y < dy; y++ {
-		p[y] = make([]uint8, dx)
-		for x := 0; x < dx; x++ {
-			p[y][x] = uint8((x+y)/2)
-		}
-	}
-	return p
+type Vertex struct {
+	Lat, Long float64
 }
 
+var m map[string]Vertex
+
 func main() {
-	pic.Show(Pic)
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
 }
