fc2763991a7602857f54fde90f8f3fbc11121b20
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Methods

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-// fibonacci returns a closure of type func() int
-func fibonacci() func() int {
-    a, b := 0, 1
-    return func() int {
-        a, b = b, a+b
-        return a
-    }
+type Vertex struct{ X, Y float64 }
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
 }
