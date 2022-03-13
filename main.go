ea21e568cde5bb7b42a544859da353be42a6c1e0
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Exercise: Web Crawler

--- main.go
+++ main.go
@@ -3,35 +3,116 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
-// SafeCounter is safe to use concurrently
-type SafeCounter struct {
-	mu sync.Mutex
-	v  map[string]int
+// Fetcher is a type that can fetch URLs and
+// return their body as a string and a slice of URLs found on that page
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on the page
+	Fetch(url string) (body string, urls []string, err error)
 }
 
-// Inc increments the counter for the given key
-func (c *SafeCounter) Inc(key string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.v[key]++
+type Result struct {
+	body  string
+	urls  []string
+	error error
 }
 
-// Value returns the current value of the counter for the given key.
-func (c *SafeCounter) Value(key string) int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.v[key]
+// Get returns a Result from the cache for a given key
+func (c *Cache) Get(key string) (Result, bool) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	item, ok := c.items[key]
+	return item, ok
+}
+
+// Set adds a Result to the cache for a given key
+func (c *Cache) Set(key string, value Result) {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	c.items[key] = value
+}
+
+type Cache struct {
+	mtx   sync.Mutex
+	items map[string]Result
+}
+
+// Crawl uses fetcher recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher, cache *Cache) {
+	// TODO: Fetch URLs in parallel
+	// TODO: Don't fetch the same URL twice
+	// this implementation doesn't do either
+	if depth <= 0 {
+		return
+	}
+	if _, ok := cache.Get(url); ok {
+		// fmt.Printf("\t<<%s is cached\n", url)
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	cache.Set(url, Result{body, urls, err})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s, %q\n", url, body)
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher, cache)
+	}
 }
 
 func main() {
-	c := SafeCounter{v: make(map[string]int)}
-	for i := 0; i < 1000; i++ {
-		go c.Inc("somekey")
-	}
+	cache := &Cache{sync.Mutex{}, make(map[string]Result)}
+	Crawl("https://golang.org/", 4, fetcher, cache)
+}
 
-	time.Sleep(time.Second)
-	fmt.Println(c.Value("somekey"))
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
 }
