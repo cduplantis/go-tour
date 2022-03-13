1f5d0eaa66ca7b8e06f55f431fbffab6ad3a43a2
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    sync.Mutex

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module caseyduplantis.com/go-tour
 
 go 1.18.0
-
-require golang.org/x/tour v0.1.0
--- main.go
+++ main.go
@@ -2,47 +2,36 @@ package main
 
 import (
 	"fmt"
-
-	"golang.org/x/tour/tree"
+	"sync"
+	"time"
 )
 
-// Walk walks the tree t sending all values
-// from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int) {
-	if t.Left != nil {
-		Walk(t.Left, ch)
-	}
-	ch <- t.Value
-	if t.Right != nil {
-		Walk(t.Right, ch)
-	}
+// SafeCounter is safe to use concurrently
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
 }
 
-// Same determines whether the trees
-// t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool {
-	ch := make(chan int)
-	ch2 := make(chan int)
-	go Walk(t1, ch)
-	go Walk(t2, ch2)
-	match := true
-	for i := 0; match && i < 10; i++ {
-		v, ok := <-ch
-		v2, ok2 := <-ch2
-		match = ok && ok2 && v == v2
-	}
-	return match
+// Inc increments the counter for the given key
+func (c *SafeCounter) Inc(key string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.v[key]++
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.v[key]
 }
 
 func main() {
-	// fmt.Printf("tree.New(1).String(): %v\n", tree.New(1).String())
-	// ch := make(chan int)
-	// go Walk(tree.New(1), ch)
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Printf("%d ch: %v\n", i, <-ch)
-	// }
-	test1 := Same(tree.New(1), tree.New(1))
-	fmt.Printf("test1: %v\n", test1)
-	test2 := Same(tree.New(1), tree.New(2))
-	fmt.Printf("test2: %v\n", test2)
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
 }
