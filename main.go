154f35f16961284ca6ecb0100424f81e23d198e7
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Exercise: Images

--- main.go
+++ main.go
@@ -1,12 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
 )
 
+type Image struct {
+	bounds image.Rectangle
+	data   [][]color.RGBA
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return i.bounds
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) At(x, y int) color.Color {
+	return i.data[y][x]
+}
+
+func NewImage(dx, dy int) *Image {
+	p := make([][]color.RGBA, dy)
+	for i := range p {
+		p[i] = make([]color.RGBA, dx)
+	}
+
+	xStep := float32(255.0) / float32(dx+dy-1)
+
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			intensity := uint8(float32(x+y) * xStep)
+			c := color.RGBA{intensity, intensity, intensity, 255}
+			p[y][x] = c
+		}
+	}
+
+	return &Image{image.Rectangle{image.Point{0, 0}, image.Point{dx, dy}}, p}
+}
+
 func main() {
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	fmt.Println(m.Bounds())
-	fmt.Println(m.At(0, 0).RGBA())
+	m := NewImage(100, 100)
+	pic.ShowImage(m)
 }
