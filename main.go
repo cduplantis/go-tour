a57f720661c0a35a0c7c39600379c9d3c027cdd0
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    For

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
-
-const (
-	Big   = 1 << 100 // high precision number
-	Small = Big >> 99
-)
-
-func needInt(x int) int           { return x*10 - 1 }
-func needFloat(x float64) float64 { return x * 0.1 }
+import "fmt"
 
 func main() {
-	fmt.Printf("needInt(Small): %v\n", needInt(Small))
-	fmt.Printf("needFloat(Small): %v\n", needFloat(Small))
-	fmt.Printf("needFloat(Big): %v\n", needFloat(Big))
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
