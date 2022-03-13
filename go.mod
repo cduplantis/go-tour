95689cf393951428bff60b335d8fdd01bcf6a1a5
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    Exercise: Maps

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module caseyduplantis.com/go-tour
 
 go 1.18.0
+
+require golang.org/x/tour v0.1.0
--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	counts := make(map[string]int)
+	for _, w := range words {
+		if v, ok := counts[w]; ok {
+			counts[w] = v + 1
+		} else {
+			counts[w] = 1
+		}
+	}
+	return counts
+}
 
 func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-	
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	wc.Test(WordCount)
 }
