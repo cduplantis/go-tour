9b97be5a80b3731e42eff159c53f65ebbe935ced
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Exercise: Readers

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"strings"
-)
+import "golang.org/x/tour/reader"
 
-func main() {
-	r := strings.NewReader("Hello, Reader!")
+type MyReader struct{}
 
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
+func (r *MyReader) Read(b []byte) (int, error) {
+  for i := range b {
+		b[i] = 'A'
 	}
+	return len(b), nil
 }
 
-
-
-
-
-
-
-
-
-
-
+func main() {
+ reader.Validate(&MyReader{})
+}
