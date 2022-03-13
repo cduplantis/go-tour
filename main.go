1aff4c11151ce45659bf8e91fa2d7debb508f207
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:44 -0500

    Generic types

--- main.go
+++ main.go
@@ -1,20 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Index[T comparable](s []T, x T) int {
-	for i, v := range s {
-		if v == x {
-			return i
-		}
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
+
+func NewList[T any](val T) *List[T] {
+	return &List[T]{val: val}
+}
+
+// implement a single linked list
+func (l *List[T]) Add(val T) {
+	if l.next == nil {
+		l.next = &List[T]{val: val}
+	} else {
+		l.next.Add(val)
 	}
-	return  -1
+}
+
+func (l *List[T]) Get(i int) T {
+	if i == 0 {
+		return l.val
+	}
+	if l.next == nil {
+		panic("index out of range")
+	}
+	return l.next.Get(i - 1)
+}
+
+func (l *List[T]) Len() int {
+	if l.next == nil {
+		return 0
+	}
+	return 1 + l.next.Len()
+}
+
+func (l *List[T]) Print() {
+	if l == nil {
+		fmt.Println()
+		return
+	}
+	fmt.Print(l.val, " ")
+	l.next.Print()
+}
+
+func (l List[T]) String() string {
+	if l.next == nil {
+		return fmt.Sprintf("%v", l.val)
+	}
+	return fmt.Sprintf("%v -> %v", l.val, l.next)
 }
 
 func main() {
-	si := []int { 10, 20, 15, -10, 0 }
-	fmt.Println(Index(si, 15))
+	l := NewList(0)
+	for i := 2; i < 10; i += 2 {
+		l.Add(i)
+	}
+	for i := 1; i < 10; i += 2 {
+		l.Add(i)
+	}
+	fmt.Printf("%s\n", l)
+	l.Print()
+	fmt.Printf("len: %d\n", l.Len())
 
-	ss := []string{"foo", "bar", "baz", "qux"}
-	fmt.Println(Index(ss, "hello"))
+	fmt.Printf("l[0]: %d\n", l.Get(0))
+	fmt.Printf("l[0]: %d\n", l.Get(4))
 }
