48183cbb53959eaf2d2e718dd973ee5e821b9ef0
Casey Duplantis <cduplantis@gmail.com>
2022-03-13 14:27:43 -0500

    If

--- main.go
+++ main.go
@@ -2,23 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+	"math"
 )
 
-func main() {
-	// monitor for CTRL-C or CTRL-Z signal and exit if received
-	fmt.Println("CTRL-C or CTRL-Z to exit")
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTSTP, syscall.SIGINT)
-	// loop endlessly
-	for {
-		// process some things
-		select {
-		case <-sig:
-			fmt.Println("Exiting...")
-			os.Exit(0)
-		}
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
 	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func main() {
+	fmt.Println(sqrt(2), sqrt(-4))
 }
